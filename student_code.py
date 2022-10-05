898741f6e840c96a4152d9d7ede091d8208024d5
Anqi Bai <baianqi086@gmail.com>
2022-10-05 15:20:00 -0400

    Update the A* search function.

--- student_code.py
+++ student_code.py
@@ -7,13 +7,9 @@ class RoutePlanner():
         self.M = M
         self.start = start
         self.goal = goal
-        self.open_list = []
+        self.frontier = []
         self.visited = set()
-        self.parent_node = dict()
-        self.node_h_value = dict()
-        self.node_g_value = dict()
-        self.node_g_value[start] = 0
-        self.node_h_value[start] = 0
+        self.came_from = dict()
 
     def find_neighbors(self, node):
         return self.M.roads[node]
@@ -26,22 +22,8 @@ class RoutePlanner():
     def get_h_value(self, node):
         return self.get_distance(node, self.goal)
 
-    def add_neighbors(self, node):
-        neighbors = self.find_neighbors(node)
-        for nei in neighbors:
-            if nei not in self.visited:
-                self.parent_node[nei] = node
-                nei_h_value = self.get_h_value(nei)
-                self.node_h_value[nei] = nei_h_value
-                nei_g_value = self.node_g_value[node] + \
-                    self.get_distance(nei, node)
-                self.node_g_value[nei] = nei_g_value
-                nei_f_value = nei_h_value + nei_g_value
-                heapq.heappush(self.open_list, (nei_f_value, nei))
-                self.visited.add(nei)
-
     def get_next_node(self):
-        node_f_value, node = heapq.heappop(self.open_list)
+        node_f_value, node = heapq.heappop(self.frontier)
         return node
 
     def construct_final_path(self, current_node):
@@ -50,21 +32,29 @@ class RoutePlanner():
         tNode = current_node
         while tNode != self.start:
             path_found.append(tNode)
-            tNode = self.parent_node[tNode]
+            tNode = self.came_from[tNode]
 
         path_found.append(self.start)
         return path_found[-1::-1]
 
-    def a_start_search(self):
+    def a_star_search(self):
         current_node = self.start
-        current_f_value = self.node_g_value[current_node] + \
-            self.node_h_value[current_node]
-
-        heapq.heappush(self.open_list, (current_f_value, current_node))
         self.visited.add(current_node)
-        while current_node != self.goal:
-            self.add_neighbors(current_node)
+        cost_so_far = {}
+        cost_so_far[self.start] = 0
+        heapq.heappush(self.frontier, (0, self.start))
+        while self.frontier:
             current_node = self.get_next_node()
+            if current_node == self.goal:
+                break
+            neighbors = self.find_neighbors(current_node)
+            for nei in neighbors:
+                new_cost = cost_so_far[current_node] + \
+                    self.get_distance(current_node, nei)
+                if nei not in cost_so_far or new_cost < cost_so_far[nei]:
+                    cost_so_far[nei] = new_cost
+                    heapq.heappush(self.frontier, (new_cost, nei))
+                    self.came_from[nei] = current_node
 
         return self.construct_final_path(self.goal)
 
@@ -74,4 +64,4 @@ def shortest_path(M, start, goal):
     # M.roads
     # start from start
     routeplanner = RoutePlanner(M, start, goal)
-    return routeplanner.a_start_search()
+    return routeplanner.a_star_search()
